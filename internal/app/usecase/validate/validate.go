ee2c5b56603d4894c42ac19b6282e199f70f7b49
Vincent <vincent_wijaya@linkaja.id>
2022-12-07 11:10:51 +0700

    update logic to compare face

--- internal/app/usecase/validate/validate.go
+++ internal/app/usecase/validate/validate.go
@@ -3,6 +3,11 @@ package validate
 import (
 	"context"
 	"errors"
+	"io"
+	"net/http"
+	"os"
+
+	recog "github.com/vincentwijaya/go-ocr/pkg/face-recog"
 
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/vincentwijaya/go-ocr/internal/app/repo/face"
@@ -64,6 +69,42 @@ func (uc *validateUC) ValidatePlateAndOwner(ctx context.Context, vehiclePhotoLoc
 	if len(faces) < 1 {
 		err = errors.New("no face data found")
 		return
+	}
+
+	var emptyByte [512]byte
+
+	for _, face := range faces {
+		// If face descriptor 0 value will try to recognize face
+		if face.Descriptor == emptyByte {
+			recog.GetFaceDescriptor(ctx)
+		}
+	}
+
+	return nil
+}
+
+func downloadAndSaveFile(URL, fileName string) error {
+	//Get the response bytes from the url
+	response, err := http.Get(URL)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return errors.New("received non 200 response code")
+	}
+	//Create a empty file
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	//Write the bytes to the fiel
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		return err
 	}
 
 	return nil
